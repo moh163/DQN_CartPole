e7e21c0d2d4ffe2d2be899647ec4a4f9ef80e5e6
moh163 <97916160+moh163@users.noreply.github.com>
2025-04-24 00:21:54 -0400

    Ajout de target network

--- DQNAgentWithTargetNetwork.py
+++ DQNAgentWithTargetNetwork.py
@@ -30,24 +30,28 @@ class DQN(nn.Module):
         x = self.relu(self.fc3(x))
         return self.out(x)
 
-class DQNAgent:
+class DQNAgentTargetNetwork:
     def __init__(self):
         self.env = TimeLimit(CustomCartPoleEnv(render_mode="rgb_array"), max_episode_steps=500)
-        self.set_seed(55) 
-        self.state_size = self.env.observation_space.shape[0]   # CartPole= 4
-        self.action_size = self.env.action_space.n               # (gauche, droite)
-        self.EPISODES = 250
+        self.set_seed(213) 
+        self.state_size = self.env.observation_space.shape[0]
+        self.action_size = self.env.action_space.n
+        self.EPISODES = 1000
         self.memory = deque(maxlen=2000)
         
-        self.gamma = 0.95      
-        self.epsilon = 1.0     
+        self.gamma = 0.95
+        self.epsilon = 1.0
         self.epsilon_min = 0.001
         self.epsilon_decay = 0.999
-        self.batch_size = 64
+        self.batch_size = 128
         self.train_start = 1000
-        
-        # Création du modèle et configuration de l'optimiseur (RMSprop avec des hyperparamètres similaires)
+
         self.model = DQN(self.state_size, self.action_size).to(device)
+        self.target_model = DQN(self.state_size, self.action_size).to(device)  
+        self.update_target_model() 
+        self.target_update_freq = 100
+        self.update_counter = 0
+
         self.optimizer = optim.RMSprop(self.model.parameters(), lr=0.00025, alpha=0.95, eps=0.01)
         self.criterion = nn.MSELoss()
     def set_seed(self, seed=seed):
@@ -66,8 +70,8 @@ class DQNAgent:
                 self.epsilon *= self.epsilon_decay
 
     # ε-greedy
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
+    def act(self, state, test=False):
+        if not test and np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
         else:
             state_tensor = torch.FloatTensor(state).to(device)  # forme (1, state_size)
@@ -75,48 +79,50 @@ class DQNAgent:
                 q_values = self.model(state_tensor)
             return np.argmax(q_values.cpu().data.numpy())
 
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
     #  experience replay sur mini-batch
     def replay(self):
         if len(self.memory) < self.train_start:
             return
         minibatch = random.sample(self.memory, min(len(self.memory), self.batch_size))
-        
-        # Préparation des batchs
+
         states = np.zeros((self.batch_size, self.state_size))
         next_states = np.zeros((self.batch_size, self.state_size))
         actions, rewards, dones = [], [], []
-        
+
         for i in range(self.batch_size):
             states[i] = minibatch[i][0]
             actions.append(minibatch[i][1])
             rewards.append(minibatch[i][2])
             next_states[i] = minibatch[i][3]
             dones.append(minibatch[i][4])
-        
+
         states = torch.FloatTensor(states).to(device)
         next_states = torch.FloatTensor(next_states).to(device)
         actions = torch.LongTensor(actions).to(device)
         rewards = torch.FloatTensor(rewards).to(device)
-        dones = torch.FloatTensor(dones).to(device)  # 1.0 si terminé, 0.0 sinon
-        
-        # Calcul des Q-values actuelles et Q-values pour l'état suivant
+        dones = torch.FloatTensor(dones).to(device)
+
         q_values = self.model(states)
-        q_next = self.model(next_states).detach()  # On ne veut pas rétropropager sur ces valeurs
-        
-        # Calcul de la cible
+        q_next = self.target_model(next_states).detach()  
+
         q_target = q_values.clone().detach()
         for i in range(self.batch_size):
             if dones[i]:
                 q_target[i][actions[i]] = rewards[i]
             else:
                 q_target[i][actions[i]] = rewards[i] + self.gamma * torch.max(q_next[i])
-        
-        # Calcul de la perte (loss) et optimisation
+
         loss = self.criterion(q_values, q_target)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+   
+        self.update_counter += 1
+        if self.update_counter % self.target_update_freq == 0:
+            self.update_target_model()
     # Sauvegarde et chargement du modèle
     def save(self, name):
         torch.save(self.model.state_dict(), name)
@@ -128,6 +134,7 @@ class DQNAgent:
     def run(self,render=True, return_rewards=False):
         start_time = time.time()
         episode_rewards = []  # Liste pour stocker la récompense cumulée de chaque épisode
+        recent_scores = deque(maxlen=2)
         for e in range(self.EPISODES):
             state, _ = self.env.reset(seed=seed)
             state = np.reshape(state, [1, self.state_size])
@@ -142,21 +149,22 @@ class DQNAgent:
                 done = terminated or truncated
                 next_state = np.reshape(next_state, [1, self.state_size])
                 # Appliquer une pénalité si l'épisode se termine prématurément
-                if done and i < self.env._max_episode_steps - 1:
-                    reward = -100
+                # if done and i < self.env._max_episode_steps - 1:
+                #     reward = -100
                 cumulative_reward += reward
                 self.remember(state, action, reward, next_state, done)
                 state = next_state
                 i += 1
                 if done:
+                    recent_scores.append(i)
                     episode_rewards.append(cumulative_reward)  # Enregistre la récompense de l'épisode
                     if render:
-                        print("Episode: {}/{}, score: {}, récompense cumulée: {:.2f}, epsilon: {:.2f}".format(
+                        print("Episode: {}/{}, score: {}, récompense cumulée: {:.2f}, epsilon: {:.4f}".format(
                             e, self.EPISODES, i, cumulative_reward, self.epsilon))
-                    # Sauvegarde du modèle avec le score max (ici 500)
-                    if i == 500:
-                        print("Saving trained model as cartpole-dqn.pth")
-                        self.save("cartpole-dqn.pth")
+                    
+                    if i==500: #len(recent_scores) == 2 and np.mean(recent_scores) == 500:
+                        print("Saving trained model as cartpole-dqnTarget.pth")
+                        self.save("cartpole-dqnTarget.pth")
                         total_training_time = time.time() - start_time  # Temps total d'entraînement
                         print(f"Temps total d'entraînement : {total_training_time:.2f} secondes")
                         if return_rewards:
@@ -178,7 +186,8 @@ class DQNAgent:
         plt.show()
     # Phase de test en chargeant un modèle sauvegardé
     def test(self):
-        self.load("cartpole-dqn.pth")
+        self.load("cartpole-dqnTarget.pth")
+        self.EPISODES = 100
         results = []  # Liste pour stocker les résultats de chaque épisode
         success_count = 0
         episode_rewards = []  # Liste pour stocker la récompense cumulée de chaque épisode
@@ -192,9 +201,7 @@ class DQNAgent:
             start_time = time.time()
             while not done:
                 self.env.render()
-                state_tensor = torch.FloatTensor(state).to(device)
-                with torch.no_grad():
-                    action = torch.argmax(self.model(state_tensor)).item()
+                action = self.act(state, test=True)
                 next_state, reward, terminated, truncated, _= self.env.step(action)
                 done = terminated or truncated
                 state = np.reshape(next_state, [1, self.state_size])
@@ -244,7 +251,7 @@ def plot_rl_results(data_dict, title, xlabel, ylabel, window_size=5, save_path=N
     else:
         plt.show()
 
-def test_hyperparameters(agent_class, hyperparams_dict, episodes=100, output_dir="plotsConstant"):
+def test_hyperparameters(agent_class, hyperparams_dict, episodes=100, output_dir="plotsConstantWithTarget"):
     for param_name, param_values in hyperparams_dict.items():
         results = {}
         print(f"\n--- Testing variations of '{param_name}' ---")
@@ -273,7 +280,7 @@ def test_hyperparameters(agent_class, hyperparams_dict, episodes=100, output_dir
 
 
 if __name__ == "__main__":
-    agent = DQNAgent()
+    agent = DQNAgentTargetNetwork()
     # Pour entraîner l'agent, décommentez la ligne suivante :
     #agent.run()
     # Pour tester l'agent avec le modèle sauvegardé, utilisez :
@@ -288,4 +295,4 @@ if __name__ == "__main__":
     #     'memory_size': [1000, 2000, 5000] 
     # }
 
-    # test_hyperparameters(DQNAgent, hyperparams_to_test, episodes=1000)
+    # test_hyperparameters(DQNAgentTargetNetwork, hyperparams_to_test, episodes=1000)
